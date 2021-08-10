99439d9030ee0203840261f8770bd52c5ad29537
Cintia95 <76740020+Cintia95@users.noreply.github.com>
2021-08-09 21:35:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mis datos</title>
+</head>
+<body>
+    <h1>Mis datos</h1>
+    <p><b>Nombre: </b><i>Cintia</i></p>
+    <p><b>Apellido: </b><i>Wdoviak</i></p>
+    <p><b>Edad: </b><i>26</i></p>
+    <p><b>Hobbie: </b><i>Leer</i></p>
+    <p><b>Editor de código preferido: </b><i>Visual Studio Code</i></p>
+    <p><b>Sistema operativo que utiliza: </b><i>Windows 10</i></p>
+</body>
+</html>
